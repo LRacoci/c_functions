d449a15fd76c22841504de9ba014e5c9da1e111b
Lucas Racoci <racoci.0@gmail.com>
2020-01-04 19:48:41 -0300

    Setting up CLion IDE

---
+++ cmake-build-debug/CMakeFiles/c_functions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_functions.dir/main.c.o"
+  "c_functions"
+  "c_functions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
